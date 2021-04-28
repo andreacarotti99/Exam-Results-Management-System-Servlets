dce690183721015f57c3296c54b1bf6813adcf23
andreacarotti <andrea1.carotti@mail.polimi.it>
2021-04-28 17:16:18 +0200

    added list of registered students to professor, security checks are still missing

---
+++ VerbEsamiProject/src/it/polimi/tiw/beans/RegisteredStudent.java
@@ -0,0 +1,86 @@
+package it.polimi.tiw.beans;
+
+public class RegisteredStudent {
+    private int id;
+    private String username;
+    private String name;
+    private String surname;
+    private String mail;
+    private Integer studentNumber;
+    private String degreeCourse;
+    private Integer mark;
+    private Integer status;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public Integer getStudentNumber() {
+        return studentNumber;
+    }
+
+    public String getDegreeCourse() {
+        return degreeCourse;
+    }
+
+    public Integer getMark() {
+        return mark;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setStudentNumber(Integer studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public void setDegreeCourse(String degreeCourse) {
+        this.degreeCourse = degreeCourse;
+    }
+
+    public void setMark(Integer mark) {
+        this.mark = mark;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
+
---
+++ VerbEsamiProject/src/it/polimi/tiw/dao/RegisteredStudentsDAO.java
@@ -0,0 +1,46 @@
+package it.polimi.tiw.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.polimi.tiw.beans.RegisteredStudent;
+
+public class RegisteredStudentsDAO {
+	private Connection connection;
+
+	public RegisteredStudentsDAO(Connection connection) {
+		this.connection = connection;
+	}
+
+	
+	public List<RegisteredStudent> findRegisteredStudentsToRound(int professorId, int roundId) throws SQLException {
+		List<RegisteredStudent> registeredStudents = new ArrayList<RegisteredStudent>();
+		
+		String query = "select r.idstudent, r.mark, r.idround, r.state, u.name, u.surname, u.email, s.studentnumber, s.degreecourse from (registered r left join user u on r.idstudent = u.id) join studentinfo s on s.id = u.id where idround = ?";
+		
+		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
+			pstatement.setInt(1, roundId);
+			try (ResultSet result = pstatement.executeQuery();) {
+				while (result.next()) {
+					
+					RegisteredStudent registeredStudent = new RegisteredStudent();
+					registeredStudent.setStudentNumber(result.getInt("s.studentnumber"));
+					registeredStudent.setSurname(result.getString("u.surname"));
+					registeredStudent.setName(result.getString("u.name"));
+					registeredStudent.setMail(result.getString("u.email"));
+					registeredStudent.setDegreeCourse(result.getString("s.degreecourse"));
+					registeredStudent.setMark(result.getInt("r.mark"));
+					registeredStudent.setStatus(result.getInt("r.state"));
+					registeredStudents.add(registeredStudent);
+
+				}
+			}
+		return registeredStudents;	
+		}
+	}
+	
+}
